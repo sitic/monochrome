dda0c351f01c0744bfe81c1500bb2543941ec2b9
Jan Lebert <jan.lebert@med.uni-goettingen.de>
2019-09-26 21:47:22 +0200

    Eigen, upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#ifdef _WIN32  // Windows 32 and 64 bit
+#ifdef _WIN32 // Windows 32 and 64 bit
 #include <windows.h>
 #endif
 
@@ -8,20 +8,68 @@
 
 #include <GLFW/glfw3.h>
 
+#include <GL/glu.h>
 #include <cstdio>
 #include <cstdlib>
+#include <fmt/format.h>
+#include <fmt/printf.h>
 #include <string>
 
-//using num = float;
-//#include "vectors.h"
+#include "Eigen/Dense"
 
-static void glfw_error_callback(int error, const char* description) {
+using pix_val_t = uint16_t;
+
+using Vec2f = Eigen::Vector2f;
+using Vec3f = Eigen::Vector3f;
+using PixArray = Eigen::Matrix<pix_val_t, Eigen::Dynamic, Eigen::Dynamic>;
+
+static void glfw_error_callback(int error, const char *description) {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-void display(GLFWwindow* window) {
+Vec3f val_to_color(const pix_val_t &val, const pix_val_t &min,
+                   const pix_val_t &max) {
+  const float factor = (max - min) / (static_cast<pix_val_t>(2));
+  const float x = (val - min) / factor - 1;
 
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  return {x, x, x};
+}
+
+void draw2dArray(const PixArray &arr, const Vec2f &position, float scale,
+                 pix_val_t min, pix_val_t max) {
+  auto image_width = arr.rows();
+  auto image_height = arr.cols();
+
+  Vec2f pos1, pos2, pos3, pos4;
+  for (long x = 0; x < image_width; x++) {
+    for (long y = 0; y < image_height; y++) {
+      pos1 = Vec2f(y, image_width - x) * scale + position;
+      pos2 = Vec2f(y + 1, image_width - x) * scale + position;
+      pos3 = Vec2f(y + 1, image_width - (x + 1)) * scale + position;
+      pos4 = Vec2f(y, image_width - (x + 1)) * scale + position;
+
+      auto val = arr(x, y);
+      auto c = val_to_color(val, min, max);
+
+      if (std::isnan(val)) {
+        c = {1, 0, 0};
+      }
+
+      glBegin(GL_QUADS);
+      glColor3fv(c.data());
+      glVertex2fv(pos1.data());
+      glVertex2fv(pos2.data());
+      glVertex2fv(pos3.data());
+      glVertex2fv(pos4.data());
+      glEnd();
+    }
+  }
+}
+
+void display(GLFWwindow *window) {
+
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
 
   // Our state
   bool show_demo_window = true;
@@ -30,10 +78,14 @@ void display(GLFWwindow* window) {
 
   while (!glfwWindowShouldClose(window)) {
     // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
+    // tell if dear imgui wants to use your inputs.
+    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to
+    // your main application.
+    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
+    // data to your main application. Generally you may always pass all inputs
+    // to dear imgui, and hide them from your application based on those two
+    // flags.
     glfwPollEvents();
 
     // Start the Dear ImGui frame
@@ -41,37 +93,52 @@ void display(GLFWwindow* window) {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    // 1. Show the big demo window (Most of the sample code is in
+    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
+    // ImGui!).
     if (show_demo_window)
       ImGui::ShowDemoWindow(&show_demo_window);
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
+    // to created a named window.
     {
       static float f = 0.0f;
       static int counter = 0;
 
-      ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
+                                     // and append into it.
 
-      ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+      ImGui::Text("This is some useful text."); // Display some text (you can
+                                                // use a format strings too)
+      ImGui::Checkbox(
+          "Demo Window",
+          &show_demo_window); // Edit bools storing our window open/close state
       ImGui::Checkbox("Another Window", &show_another_window);
 
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+      ImGui::SliderFloat("float", &f, 0.0f,
+                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+      ImGui::ColorEdit3(
+          "clear color",
+          (float *)&clear_color); // Edit 3 floats representing a color
 
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+      if (ImGui::Button("Button")) // Buttons return true when clicked (most
+                                   // widgets return true when edited/activated)
         counter++;
       ImGui::SameLine();
       ImGui::Text("counter = %d", counter);
 
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                  1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
       ImGui::End();
     }
 
     // 3. Show another simple window.
-    if (show_another_window)
-    {
-      ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+    if (show_another_window) {
+      ImGui::Begin(
+          "Another Window",
+          &show_another_window); // Pass a pointer to our bool variable (the
+                                 // window will have a closing button that will
+                                 // clear the bool when clicked)
       ImGui::Text("Hello from another window!");
       if (ImGui::Button("Close Me"))
         show_another_window = false;
@@ -86,68 +153,85 @@ void display(GLFWwindow* window) {
     glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
-    // you may need to backup/reset/restore current shader using the commented lines below.
-    //GLint last_program;
-    //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-    //glUseProgram(0);
+    // If you are using this code with non-legacy OpenGL header/contexts (which
+    // you should not, prefer using imgui_impl_opengl3.cpp!!), you may need to
+    // backup/reset/restore current shader using the commented lines below.
+    // GLint last_program;
+    // glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+    // glUseProgram(0);
     ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-    //glUseProgram(last_program);
+    // glUseProgram(last_program);
 
     // Update and Render additional Platform Windows
-    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-      GLFWwindow* backup_current_context = glfwGetCurrentContext();
+    // (Platform functions may change the current OpenGL context, so we
+    // save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call
+    //  glfwMakeContextCurrent(window) directly)
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      GLFWwindow *backup_current_context = glfwGetCurrentContext();
       ImGui::UpdatePlatformWindows();
       ImGui::RenderPlatformWindowsDefault();
       glfwMakeContextCurrent(backup_current_context);
     }
 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();							// Reset The Projection Matrix
+    gluOrtho2D(0,600,0,600);
+    glMatrixMode(GL_MODELVIEW);						// Select The Modelview Matrix
+    glLoadIdentity();							// Reset The Modelview Matrix
+
+    Vec2f offset = {0, 0};
+    PixArray arr = PixArray::Random(512, 512);
+    draw2dArray(arr, offset, 1, 0, std::numeric_limits<pix_val_t>::max());
+
     glfwSwapBuffers(window);
   }
 }
 
-void drop_callback(GLFWwindow* window, int count, const char** paths) {
-  for (int i = 0;  i < count;  i++) {
+void drop_callback(GLFWwindow *window, int count, const char **paths) {
+  for (int i = 0; i < count; i++) {
     std::string path = paths[i];
     fprintf(stderr, "drop %d: %s\n", i, path.c_str());
   }
 }
 
-int main(int, char**) {
-  GLFWwindow* window;
+int main(int, char **) {
+  GLFWwindow *window;
   glfwSetErrorCallback(glfw_error_callback);
   if (!glfwInit())
     exit(EXIT_FAILURE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+  window = glfwCreateWindow(640, 640, "Simple example", NULL, NULL);
   if (!window) {
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
   glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);  // wait until the current frame has been drawn before drawing the next one
+  glfwSwapInterval(1); // wait until the current frame has been drawn before
+                       // drawing the next one
 
   ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-  //io.ConfigViewportsNoAutoMerge = true;
-  //io.ConfigViewportsNoTaskBarIcon = true;
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad
+  // Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;   // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport /
+                                                      // Platform Windows
+  // io.ConfigViewportsNoAutoMerge = true;
+  // io.ConfigViewportsNoTaskBarIcon = true;
 
   // Setup Dear ImGui style
   ImGui::StyleColorsDark();
-  //ImGui::StyleColorsClassic();
+  // ImGui::StyleColorsClassic();
 
-  // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-  ImGuiStyle& style = ImGui::GetStyle();
-  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-  {
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform
+  // windows can look identical to regular ones.
+  ImGuiStyle &style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
     style.WindowRounding = 0.0f;
     style.Colors[ImGuiCol_WindowBg].w = 1.0f;
   }
@@ -159,19 +243,28 @@ int main(int, char**) {
   glfwSetDropCallback(window, drop_callback);
 
   // Load Fonts
-  // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-  // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-  // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+  // - If no fonts are loaded, dear imgui will use the default font. You can
+  // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
+  // them.
+  // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
+  // need to select the font among multiple.
+  // - If the file cannot be loaded, the function will return NULL. Please
+  // handle those errors in your application (e.g. use an assertion, or display
+  // an error and quit).
+  // - The fonts will be rasterized at a given size (w/ oversampling) and stored
+  // into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which
+  // ImGui_ImplXXXX_NewFrame below will call.
   // - Read 'misc/fonts/README.txt' for more instructions and details.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-  //io.Fonts->AddFontDefault();
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-  //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-  //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-  //IM_ASSERT(font != NULL);
+  // - Remember that in C/C++ if you want to include a backslash \ in a string
+  // literal you need to write a double backslash \\ !
+  // io.Fonts->AddFontDefault();
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+  // ImFont* font =
+  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+  // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
 
   display(window);
 
