993f74b936d5433aa902314b6cba9716480a5000
Jan Lebert <jan.lebert@med.uni-goettingen.de>
2020-11-12 14:32:14 +0100

    Fix python flow test

--- python/tests/test_flow.py
+++ python/tests/test_flow.py
@@ -1,5 +1,5 @@
 import numpy as np
-from quickVidViewer import open_array3, open_flow
+from quickVidViewer import open_array, open_flow
 
 vid = np.zeros((100, 128, 128), dtype=np.float32)
 vid[:, :64:2, :64:2] = 1
@@ -9,5 +9,5 @@ for t in range(100):
     flow[t, :64, :64, 0] = t / 100
     flow[t, :64, :64, 1] = t / 100
 
-open_array3(vid)
+open_array(vid)
 open_flow(flow, "")
