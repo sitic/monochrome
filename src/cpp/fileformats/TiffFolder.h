217a8f96b1ae7365eeb3b5199cc8f7e7085704fb
Jan Lebert <jan.lebert@ucsf.edu>
2025-03-15 14:45:40 -0700

    Add native TiffReaders

---
+++ src/cpp/fileformats/TiffFolder.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <strnatcmp.h>
+
+#include "TiffFolder.h"
+
+class TiffFolder : public AbstractFile {
+ private:
+  bool _good = true;
+  std::string _error_msg;
+  std::vector<std::shared_ptr<TiffFile>> _files;
+
+  int _nx = 0, _ny = 0, _nc = 0, _nt = 0;
+  std::string _date    = "";
+  std::string _comment = "";
+
+
+ public:
+  TiffFolder(fs::path path) : AbstractFile(path) {
+    std::vector<fs::path> tiff_files;
+    const std::unordered_set<std::string> extensions = {".tif", ".tiff", ".TIF", ".TIFF"};
+    for (const auto& entry : fs::directory_iterator(path)) {
+      if (entry.is_regular_file() && extensions.count(entry.path().extension().string()) > 0) {
+        tiff_files.push_back(entry.path());
+      }
+    }
+    std::sort(tiff_files.begin(), tiff_files.end(), [](const fs::path& a, const fs::path& b) {
+      return strnatcmp(a.stem().c_str(), b.stem().c_str()) < 0;
+    });
+    for (const auto& entry : tiff_files) {
+      auto file = std::make_shared<TiffFile>(entry, true);
+      _files.push_back(file);
+      if (!file->good()) {
+        _error_msg = "Failed to load TIFF file: " + entry.string();
+        _good      = false;
+        break;
+      }
+      if (file->length() != 1) {
+        _error_msg = "Image folder contains multiple frames per file";
+        _good      = false;
+        break;
+      }
+    }
+
+    if (_good) {
+      _nx      = _files[0]->Nx();
+      _ny      = _files[0]->Ny();
+      _nc      = _files[0]->Nc();
+      _nt      = _files.size();
+      _date    = _files[0]->date();
+      _comment = _files[0]->comment();
+    }
+    for (const auto& file : _files) {
+      if (file->length() != 1) {
+        _error_msg = "Image folder contains multiple frames per file";
+        _good      = false;
+        break;
+      }
+      if (file->Nx() != _nx || file->Ny() != _ny || file->Nc() != _nc) {
+        _error_msg = "Inconsistent dimensions in TIFF files";
+        _good      = false;
+        break;
+      }
+    }
+  }
+
+
+  bool good() const final { return _good; }
+
+  std::string error_msg() final { return _error_msg; }
+
+  int Nx() const final { return _nx; }
+  int Ny() const final { return _ny; }
+  int Nc() const final { return _nc; }
+  int length() const final { return _nt; }
+
+  std::string date() const final { return _date; }
+  std::string comment() const final { return _comment; }
+  std::chrono::duration<float> duration() const final { return 0s; }
+  float fps() const final { return 0; }
+  std::vector<std::pair<std::string, std::string>> metadata() const final {
+    if (!_good) {
+      return {};
+    } else {
+      return _files[0]->metadata();
+    }
+  }
+  std::optional<BitRange> bitrange() const final {
+    if (!_good) {
+      return std::nullopt;
+    } else {
+      return _files[0]->bitrange();
+    }
+  }
+  std::optional<ColorMap> cmap() const final {
+    if (!_good) {
+      return std::nullopt;
+    } else {
+      return _files[0]->cmap();
+    }
+  }
+
+  Eigen::MatrixXf read_frame(long t, long c) final {
+    if (t < 0 || t >= length()) {
+      _error_msg = "Time index out of range";
+      _good      = false;
+      return Eigen::MatrixXf();
+    }
+    return _files[t]->read_frame(0, c);
+  }
+
+  float get_pixel(long t, long x, long y) final {
+    if (t < 0 || t >= length()) {
+      _error_msg = "Time index out of range";
+      _good      = false;
+      return 0;
+    }
+    return _files[t]->get_pixel(0, x, y);
+  }
+
+  //No support for these
+  flag_set<FileCapabilities> capabilities() const final { return {}; }
+  void set_comment(const std::string& new_comment) final {}
+};
--- src/cpp/fileformats/all_formats.cpp
+++ src/cpp/fileformats/all_formats.cpp
@@ -10,6 +10,8 @@
 #include "BmpFile.h"
 #include "RawFile.h"
 #include "NpyFile.h"
+#include "TiffFile.h"
+#include "TiffFolder.h"
 
 #include "all_formats.h"
 #include "utils/files.h"
@@ -19,29 +21,29 @@ namespace {
 
   bool substring_replace(std::string& str, const std::string& from, const std::string& to) {
     size_t start_pos = str.find(from);
-    if(start_pos == std::string::npos)
-        return false;
+    if (start_pos == std::string::npos) return false;
     str.replace(start_pos, from.length(), to);
     return true;
   }
-  
+
   std::string random_alphanumeric_string(size_t length) {
-      thread_local std::mt19937 rng{std::random_device{}()};
-      auto randchar = []() -> char {
-          const char charset[] =
+    thread_local std::mt19937 rng{std::random_device{}()};
+    auto randchar = []() -> char {
+      const char charset[] =
           "0123456789"
           "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
           "abcdefghijklmnopqrstuvwxyz";
-          const size_t max_index = (sizeof(charset) - 1);
-          std::uniform_int_distribution<size_t> dist(0, max_index - 1);
-          return charset[dist(rng)];
-      };
-      std::string str(length, 0);
-      std::generate_n(str.begin(), length, randchar);
-      return str;
+      const size_t max_index = (sizeof(charset) - 1);
+      std::uniform_int_distribution<size_t> dist(0, max_index - 1);
+      return charset[dist(rng)];
+    };
+    std::string str(length, 0);
+    std::generate_n(str.begin(), length, randchar);
+    return str;
   }
 
-  bool directory_has_files_with_extension(const fs::path& path, const std::unordered_set<std::string>& extensions) {
+  bool directory_has_files_with_extension(const fs::path& path,
+                                          const std::unordered_set<std::string>& extensions) {
     for (const auto& entry : fs::directory_iterator(path)) {
       if (entry.is_regular_file() && extensions.count(entry.path().extension().string()) > 0) {
         return true;
@@ -49,31 +51,40 @@ namespace {
     }
     return false;
   }
-}
+}  // namespace
 
 std::shared_ptr<AbstractFile> file_factory(const fs::path& path) {
   std::shared_ptr<AbstractFile> file;
 
   // Directory loaders
   if (fs::is_directory(path)) {
+    // Native directory handlers
+    if (directory_has_files_with_extension(path, {".tif", ".tiff", ".TIF", ".TIFF"})) {
+      file = std::make_shared<TiffFolder>(path);
+      if (!file->good()) {
+        // Load failed, let the "load_folder.py" plugin handle it
+        file = nullptr;
+      }
+    }
     // Python-based directory handlers
     using file_extension_vt = std::unordered_set<std::string>;
     const std::unordered_map<std::string, file_extension_vt> folder_handlers = {
-      {"load_folder.py", {".png", ".tif", ".tiff", ".PNG", ".TIF", ".TIFF"}},
-      {"load_folder_dcm.py", {".dcm", ".DCM"}},
+        {"load_folder.py", {".png", ".tif", ".tiff", ".PNG", ".TIF", ".TIFF"}},
+        {"load_folder_dcm.py", {".dcm", ".DCM"}},
     };
-    for (const auto& [script_name, extensions] : folder_handlers) {
-      if (directory_has_files_with_extension(path, extensions)) {
-        python_plugin_load(path, script_name);
-        return nullptr;
+    if (!file) {
+      for (const auto& [script_name, extensions] : folder_handlers) {
+        if (directory_has_files_with_extension(path, extensions)) {
+          python_plugin_load(path, script_name);
+          return nullptr;
+        }
       }
+      global::new_ui_message(
+          "ERROR: Unable to load directory, no TIFF or PNG files found.\nPath: {}", path.string());
+      return nullptr;
     }
-    global::new_ui_message(
-      "ERROR: Unable to load directory, no TIFF or PNG files found.\nPath: {}",
-      path.string());
-    return nullptr;
   }
-  
+
   // File loaders
   std::string extension = path.extension().string();
   std::transform(extension.begin(), extension.end(), extension.begin(),
@@ -90,53 +101,72 @@ std::shared_ptr<AbstractFile> file_factory(const fs::path& path) {
     if (!file->good()) {
       file = std::make_shared<BmpFile>(path);
     }
-  } else {
-    // Python-based file handlers
+  } else if (extension == ".tif" || extension == ".tiff") {
+    file = std::make_shared<TiffFile>(path);
+    if (!file->good()) {
+      fmt::print("TiffFile load failed, will use python plugin instead. Error: {}\n",
+                 file->error_msg());
+      // Load failed, let the "load_tiff.py" plugin handle it
+      file = nullptr;
+    }
+  }
+  // Python plugin based file handlers
+  if (!file) {
     const std::unordered_map<std::string, std::string> python_handlers = {
-      {".tif", "load_tiff.py"},
-      {".tiff", "load_tiff.py"},
-      {".lsm", "load_tiff.py"},
-      {".stk", "load_tiff.py"},
-      {".mat", "load_mat.py"},
-      {".png", "load_image.py"},
-      {".jpg", "load_image.py"},
-      {".jpeg", "load_image.py"},
-      {".webp", "load_image.py"},
-      {".bmp", "load_image.py"},
-      {".dcm", "load_image_itk.py"},
-      {".dicom", "load_image_itk.py"},
-      {".gdcm", "load_image_itk.py"},
-      {".gipl", "load_image_itk.py"},
-      {".hdf5", "load_image_itk.py"},
-      {".hdr", "load_image_itk.py"},
-      {".ipl", "load_image_itk.py"},
-      {".img", "load_image_itk.py"},
-      {".img.nz", "load_image_itk.py"},
-      {".mgh", "load_image_itk.py"},
-      {".mha", "load_image_itk.py"},
-      {".mhd", "load_image_itk.py"},
-      {".mnc", "load_image_itk.py"},
-      {".mnc2", "load_image_itk.py"},
-      {".nhdr", "load_image_itk.py"},
-      {".nia", "load_image_itk.py"},
-      {".nii", "load_image_itk.py"},
-      {".nii.gz", "load_image_itk.py"},
-      {".nrrd", "load_image_itk.py"},
-      {".vtk", "load_image_itk.py"},
-      {".mp4", "load_encoded_video.py"},
-      {".avi", "load_encoded_video.py"},
-      {".webm", "load_encoded_video.py"},
-      {".mov", "load_encoded_video.py"},
-      {".mkv", "load_encoded_video.py"},
-      {".gif", "load_encoded_video.py"},
-      {".wmv", "load_encoded_video.py"},
-      {".m4v", "load_encoded_video.py"},
-      {".gsd", "load_micam.py"},
-      {".gsh", "load_micam.py"},
-      {".rsh", "load_micam.py"},
-      {".rsm", "load_micam.py"},
-      {".rsd", "load_micam.py"},
-      {".zzz_test", "load_plugin_test.py"}, // For testing purposes
+        {".tif", "load_tiff.py"},
+        {".tiff", "load_tiff.py"},
+        {".tf8", "load_tiff.py"},
+        {".ptif", "load_tiff.py"},
+        {".ptiff", "load_tiff.py"},
+        {".lsm", "load_tiff.py"},  // ZEISS LSM
+        {".btf", "load_tiff.py"},
+        {".bif", "load_tiff.py"},  // Roche Digital Pathology
+        {".gel", "load_tiff.py"},
+        {".ndpi", "load_tiff.py"}, // Hamamatsu Slide Scanner
+        {".stk", "load_tiff.py"},
+        {".qptiff", "load_tiff.py"},  // Perkin Elmer Vectra
+        // TODO: https://github.com/cgohlke/liffile
+        {".mat", "load_mat.py"},
+        {".png", "load_image.py"},
+        {".jpg", "load_image.py"},
+        {".jpeg", "load_image.py"},
+        {".webp", "load_image.py"},
+        {".bmp", "load_image.py"},
+        {".pgm", "load_image.py"},
+        {".dcm", "load_image_itk.py"},
+        {".dicom", "load_image_itk.py"},
+        {".gdcm", "load_image_itk.py"},
+        {".gipl", "load_image_itk.py"},
+        {".hdf5", "load_image_itk.py"},
+        {".hdr", "load_image_itk.py"},
+        {".ipl", "load_image_itk.py"},
+        {".img", "load_image_itk.py"},
+        {".img.nz", "load_image_itk.py"},
+        {".mgh", "load_image_itk.py"},
+        {".mha", "load_image_itk.py"},
+        {".mhd", "load_image_itk.py"},
+        {".mnc", "load_image_itk.py"},
+        {".mnc2", "load_image_itk.py"},
+        {".nhdr", "load_image_itk.py"},
+        {".nia", "load_image_itk.py"},
+        {".nii", "load_image_itk.py"},
+        {".nii.gz", "load_image_itk.py"},
+        {".nrrd", "load_image_itk.py"},
+        {".vtk", "load_image_itk.py"},
+        {".mp4", "load_encoded_video.py"},
+        {".avi", "load_encoded_video.py"},
+        {".webm", "load_encoded_video.py"},
+        {".mov", "load_encoded_video.py"},
+        {".mkv", "load_encoded_video.py"},
+        {".gif", "load_encoded_video.py"},
+        {".wmv", "load_encoded_video.py"},
+        {".m4v", "load_encoded_video.py"},
+        {".gsd", "load_micam.py"},
+        {".gsh", "load_micam.py"},
+        {".rsh", "load_micam.py"},
+        {".rsm", "load_micam.py"},
+        {".rsd", "load_micam.py"},
+        {".zzz_test", "load_plugin_test.py"},  // For testing purposes
     };
     auto it = python_handlers.find(extension);
     if (it == python_handlers.end()) {
@@ -146,9 +176,8 @@ std::shared_ptr<AbstractFile> file_factory(const fs::path& path) {
       python_plugin_load(path, it->second);
       return nullptr;
     } else {
-      global::new_ui_message(
-          "ERROR: Unable to load file, it has an unknown extension.\nFile: {}",
-          path.string());
+      global::new_ui_message("ERROR: Unable to load file, it has an unknown extension.\nFile: {}",
+                             path.string());
       return nullptr;
     }
   }
@@ -181,40 +210,45 @@ std::string create_npy_fileheader_float(std::vector<unsigned long> shape) {
 }
 
 namespace {
-void python_plugin_load(const fs::path& path, std::string script_name) {
-  // Load the script template and save it to a temporary file
-  std::string script = utils::get_rc_text_file("src/python/embedded_plugins/" + script_name);
-  if (!substring_replace(script, "\"monochrome\",",
-                         fmt::format("\"monochrome=={}\",", MONOCHROME_VERSION)) ||
-      !substring_replace(script, "sys.argv[1]",
-                         fmt::format("r\"{}\"", fs::absolute(path).string()))) {
-    global::new_ui_message("ERROR: Unable format plugin script {}", script_name);
-    return;
-  }
-#ifdef _WIN32
-  while (substring_replace(script, "\r\n", "\n")) {};
-#endif
-  auto script_fn = fs::temp_directory_path() /
-                   fmt::format("monochrome_{}_{}.py", script_name.substr(0, script_name.find('.')),
-                               random_alphanumeric_string(5));
-  if (!utils::write_text_file(script_fn, script)) {
-    return;
-  }
-
-  auto builder = subprocess::RunBuilder({utils::get_uv_executable(), "run", script_fn.string()});
-  auto env = subprocess::current_env_copy();
-  env["PYTHONUNBUFFERED"] = "1";
-  builder.env(env);
-  std::string title = fmt::format("Loading file {}", path.string());
-  std::string msg = fmt::format(
-      "Loading '{}'...\n\n"
-      "Required plugin dependencies will be downloaded automatically."
-      " This may take some time on first import.", 
-      path.filename().string());
-  global::add_subprocess(builder, title, msg, [script_fn]() {
-    if (fs::exists(script_fn)) {
-      fs::remove(script_fn);
+  void python_plugin_load(const fs::path& path, std::string script_name) {
+    // Load the script template and save it to a temporary file
+    std::string script = utils::get_rc_text_file("src/python/embedded_plugins/" + script_name);
+    // auto req_str = fmt::format("\"monochrome=={}\",", MONOCHROME_VERSION);
+    auto req_str = fmt::format(
+        "\"monochrome @ "
+        "/Users/janl/work/software/monochrome/dist/"
+        "monochrome-2025.2.21.dev59+g80f725e8c.d20250314-py3-none-macosx_15_0_arm64.whl\",");
+    if (!substring_replace(script, "\"monochrome\",", req_str) ||
+        !substring_replace(script, "sys.argv[1]",
+                           fmt::format("r\"{}\"", fs::absolute(path).string()))) {
+      global::new_ui_message("ERROR: Unable format plugin script {}", script_name);
+      return;
     }
-  });
-}
-}
+#ifdef _WIN32
+    while (substring_replace(script, "\r\n", "\n")) {
+    };
+#endif
+    auto script_fn = fs::temp_directory_path() /
+                     fmt::format("monochrome_{}_{}.py", script_name.substr(0, script_name.find('.')),
+                                 random_alphanumeric_string(5));
+    if (!utils::write_text_file(script_fn, script)) {
+      return;
+    }
+
+    auto builder = subprocess::RunBuilder({utils::get_uv_executable(), "run", script_fn.string()});
+    auto env     = subprocess::current_env_copy();
+    env["PYTHONUNBUFFERED"] = "1";
+    builder.env(env);
+    std::string title = fmt::format("Loading file {}", path.string());
+    std::string msg   = fmt::format(
+        "Loading '{}'...\n\n"
+          "Required plugin dependencies will be downloaded automatically."
+          " This may take some time on first import.",
+        path.filename().string());
+    global::add_subprocess(builder, title, msg, [script_fn]() {
+      if (fs::exists(script_fn)) {
+        fs::remove(script_fn);
+      }
+    });
+  }
+}  // namespace
